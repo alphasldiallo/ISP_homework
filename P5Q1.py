5d7c5d1d6d5a556d566760a001e3f58b2d9a95c6
Alpha Mamadou Diallo <alphamamadou.diallo@unil.ch>
2018-10-16 16:55:36 +0200

    I changed the dict by an array

--- P5Q1.py
+++ P5Q1.py
@@ -7,10 +7,15 @@ import operator
 ctext ="Bm bl ngdghpg ahp xyyxvmbox max Vtxltk vbiaxk ptl tm max mbfx, unm bm bl ebdxer mh atox uxxg kxtlhgtuer lxvnkx, ghm extlm uxvtnlx fhlm hy Vtxltk'l xgxfbxl phnew atox uxxg beebmxktmx tgw hmaxkl phnew atox tllnfxw matm max fxlltzxl pxkx pkbmmxg bg tg ngdghpg yhkxbzg etgzntzx. Maxkx bl gh kxvhkw tm matm mbfx hy tgr mxvagbjnxl yhk max lhenmbhg hy lbfiex lnulmbmnmbhg vbiaxkl. Max xtkebxlm lnkobobgz kxvhkwl wtmx mh max 9ma vxgmnkr phkdl hy Te-Dbgwb bg max Tktu phkew pbma max wblvhoxkr hy ykxjnxgvr tgterlbl."
 letter_frequency_english =  {'e','t','a','o','i','n','s','r','h','d','l','u','c','m','f','y','w','g','p','b','v','k','x','q','j','z'}
 alphabet =  {'a':'0','b':'1','c':'2','d':'3','e':'4','f':'5','g':'6','h':'7','i':'8','j':'9','k':'10','l':'11','m':'12','n':'13','o':'14','p':'15','q':'16','r':'17','s':'18','t':'19','u':'20','v':'21','w':'22','x':'23','y':'24','z':'25'}
+
+alphabet_array = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
 alphabet_1 =  ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 c_special =['!', ''" "'','"', '#', '$', '%', '&', '(',')', '*' ,'+',',', '_','.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':',';','=', '?', '@', '[', ']','', '^', '_', '{', '|', '}', '~',"'", '-']
 dico_ct ={}
 result1 = ()
+
+print ("alphabet", alphabet_array[5])
 
 for letter in ctext.lower():
    if letter not in c_special:
@@ -20,6 +25,7 @@ for letter in ctext.lower():
           dico_ct[letter] += 1
 print(dico_ct)
 
+# Order the elements of the list
 result= sorted(dico_ct.items(), key=operator.itemgetter(1), reverse=True)
 print("result",(result[0]))
 
@@ -38,9 +44,12 @@ print (int(alphabet[(list[0])]))
 #    print("LT", lt)
  #   b = ((int(alphabet[list[0]])) - (int(alphabet[alphabet_1[4]]))) % 26
 
+print ("position ", alphabet_array.index("f"))
 
-b = ((int(alphabet[list[0]]))-(int(alphabet['e'])))%26
-print ("B = ",b)
+for i in alphabet_array:
+    b = ((int(alphabet[list[0]]))-(int(alphabet(alphabet_array[i]))))%26
+    b = 19
+    #print ("B = ",b)
 
 
 result1 = alphabet_1[b:] + alphabet_1[:b]
